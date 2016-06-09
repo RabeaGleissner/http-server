3478500afe66d77bd0243e3cc5b509dd9766ce29
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-09 08:53:54 +0100

    adds log

---
+++ src/main/java/de/rabea/request/Log.java
@@ -0,0 +1,20 @@
+package de.rabea.request;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Log {
+
+    private List<String> requests;
+    public Log() {
+        this.requests =  new LinkedList<>();
+    }
+
+    public void register(String requestHead) {
+
+    }
+
+    public String show() {
+        return null;
+    }
+}
