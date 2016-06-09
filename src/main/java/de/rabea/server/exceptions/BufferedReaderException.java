1d4441e30439bf7f197fab4c149e0792bc55f709
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-09 14:35:16 +0100

    removes BufferedReaderExcpetion

--- src/main/java/de/rabea/server/exceptions/BufferedReaderException.java
+++
@@ -1,8 +0,0 @@
-package de.rabea.server.exceptions;
-
-public class BufferedReaderException extends RuntimeException {
-
-    public BufferedReaderException(String message) {
-        super(message);
-    }
-}
