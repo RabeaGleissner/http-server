54bebc32e0bf0b9be51bdacb87976cc40a2b017a
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-14 17:44:51 +0100

    uses junit temp file rule instead of real file for testing

--- src/test/java/de/rabea/request/FileParserTest.java
+++ src/test/java/de/rabea/request/FileParserTest.java
@@ -1,47 +1,68 @@
 package de.rabea.request;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static de.rabea.TestHelper.*;
 import static org.junit.Assert.assertEquals;
 
 public class FileParserTest {
-    private final String file = directory() + "/file.txt";
+    private String pathToFile;
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
 
     @Before
-    public void setup() {
+    public void setup() throws IOException {
+        pathToFile = createFileWithContent().getAbsolutePath();
         resetFileContent();
     }
 
     @Test
-    public void readFileContents() {
-        FileParser fileParser = new FileParser(file);
+    public void readFileContents() throws IOException {
+        FileParser fileParser = new FileParser(pathToFile);
         assertEquals("Some content", asString(fileParser.read()));
     }
 
     @Test
     public void readsPartialContentIfRangeIsGiven() {
-        FileParser fileParser = new FileParser(file, "0-4");
+        FileParser fileParser = new FileParser(pathToFile, "0-4");
         assertEquals("Some ", asString(fileParser.read()));
     }
 
     @Test
     public void readsPartialContentWithOnlyRangeEndGiven() {
-        FileParser fileParser = new FileParser(file, "-6");
+        FileParser fileParser = new FileParser(pathToFile, "-6");
         assertEquals("ontent", asString(fileParser.read()));
     }
 
     @Test
     public void readsPartialContentWithOnlyRangeStartGiven() {
-        FileParser fileParser = new FileParser(file, "4-");
+        FileParser fileParser = new FileParser(pathToFile, "4-");
         assertEquals(" content", asString(fileParser.read()));
     }
 
     @Test
     public void updatesFileWithGivenContent() {
-        FileParser fileParser = new FileParser(file);
+        FileParser fileParser = new FileParser(pathToFile);
         fileParser.updateExistingFile("updated");
         assertEquals("updated", asString(fileParser.read()));
     }
+
+    private File createFileWithContent() throws IOException {
+        File testFile = testFolder.newFile("file.txt");
+        FileWriter writer = new FileWriter(testFile.getAbsoluteFile());
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        bufferedWriter.write("Some content");
+        bufferedWriter.close();
+        return testFile;
+    }
+
 }
